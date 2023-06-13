c2500e1c8db80d17d8dbadccfba8dce4d8b43ad7
KuBaN_ <garkovski_dmitri@mail.ru>
2023-06-13 18:42:05 +0300

    вычисляемое поле в админке

--- moviesite/movieapp/admin.py
+++ moviesite/movieapp/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Movie
 
-# Register your models here.
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['name', 'rating', 'year', 'budget', 'slug', 'rating_status']
+    list_editable = ['rating', 'year', 'budget']
+    ordering = ['-rating', 'name']
+    list_per_page = 15
+
+    @admin.display(ordering='rating', description='статус')
+    def rating_status(self, movie: Movie):
+        if movie.rating < 70:
+            return 'Зачем это смотреть'
+        if movie.rating < 80:
+            return 'Разок можно глянуть'
+        if movie.rating < 85:
+            return 'Зачет'
+        return 'Топчик'
--- moviesite/movieapp/models.py
+++ moviesite/movieapp/models.py
@@ -6,11 +6,12 @@ from django.utils.text import slugify
 class Movie(models.Model):
     name = models.CharField(max_length=40)
     rating = models.IntegerField()
-    year = models.IntegerField(null=True)
+    year = models.IntegerField(null=True, blank=True)
     budget = models.IntegerField(null=True)
     slug = models.SlugField(default='',
                             null=False,
-                            db_index=True) # для быстрого поиска в DB
+                            db_index=True,
+                            blank=True)# для быстрого поиска в DB
 
     def get_url(self):
         return reverse('movie-detail', args=[self.slug])
