65d60734434e7722884c5ee6faa0e853da8d7532
KuBaN23_ <garkovski_dmitri@mail.ru>
2023-06-14 19:27:14 +0300

    ModelForm

--- form_project/feedback/forms.py
+++ form_project/feedback/forms.py
@@ -1,13 +1,32 @@
 from django import forms
+from .models import Feedback
 
+# class FeedbackForm(forms.Form):
+#     name = forms.CharField(label='Имя:', max_length=13,
+#                            min_length=4, error_messages={
+#             'max_length': 'Слишком много символов',
+#             'min_length': 'Слишком мало символов',
+#             'requered': 'Укажите хотя бы один символ'
+#         })
+#     surname = forms.CharField()
+#     feedback = forms.CharField(widget=forms.Textarea(attrs={'rows': 2, 'cols': 20}))
+#     rating = forms.IntegerField(label='Рейтинг',min_value=0, max_value=5)
 
-class FeedbackForm(forms.Form):
-    name = forms.CharField(label='Имя:', max_length=13,
-                           min_length=4, error_messages={
-            'max_length': 'Слишком много символов',
-            'min_length': 'Слишком мало символов',
-            'requered': 'Укажите хотя бы один символ'
-        })
-    surname = forms.CharField()
-    feedback = forms.CharField(widget=forms.Textarea(attrs={'rows': 2, 'cols': 20}))
-    rating = forms.IntegerField(label='Рейтинг',min_value=0, max_value=5)
+class FeedbackForm(forms.ModelForm):
+    class Meta:
+        model = Feedback
+        # fields = ['name', 'rating', 'surname', 'feedback']
+        fields = '__all__'
+        labels = {
+            'name': 'Имя',
+            'surname': 'Фамилия',
+            'feedback': 'Отзыв',
+            'rating': 'Рэйтинг',
+        }
+        error_messages = {
+            'name': {
+                'max_length': '* ой как много символов',
+                'min_length': '* ой как мало символов',
+                'required': '* поле не должно быть пустым',
+            }
+        }
