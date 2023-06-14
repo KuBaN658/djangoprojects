ecaf761f2a44a220bd4924b210a654ab97b98eda
KuBaN23_ <garkovski_dmitri@mail.ru>
2023-06-14 18:18:10 +0300

    сохранение данных от пользователя

--- form_project/feedback/views.py
+++ form_project/feedback/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import FeedbackForm
+from .models import Feedback
+
 
 def index(request):
 
     if request.method == 'POST':
         form = FeedbackForm(request.POST)
         if form.is_valid():
+            print(form.cleaned_data)
+            feed = Feedback(
+                name=form.cleaned_data['name'],
+                surname=form.cleaned_data['surname'],
+                feedback=form.cleaned_data['feedback'],
+                rating=form.cleaned_data['rating'],
+            )
+            feed.save()
             return HttpResponseRedirect('/done')
     else:
         form = FeedbackForm()
