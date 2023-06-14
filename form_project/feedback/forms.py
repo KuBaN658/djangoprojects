640836d0943bb23777f159b063df9a5a7475fdf1
KuBaN23_ <garkovski_dmitri@mail.ru>
2023-06-14 17:28:08 +0300

    добавление стилей

--- form_project/feedback/forms.py
+++ form_project/feedback/forms.py
@@ -8,6 +8,6 @@ class FeedbackForm(forms.Form):
             'min_length': 'Слишком мало символов',
             'requered': 'Укажите хотя бы один символ'
         })
-    # surname = forms.CharField()
-    # feedback = forms.CharField(widget=forms.Textarea(attrs={'rows': 2, 'cols': 20}))
+    surname = forms.CharField()
+    feedback = forms.CharField(widget=forms.Textarea(attrs={'rows': 2, 'cols': 20}))
     rating = forms.IntegerField(label='Рейтинг',min_value=0, max_value=5)
