55317acfaee440ed5fd446c515dfc999c17bf19a
KuBaN_ <garkovski_dmitri@mail.ru>
2023-06-15 12:04:07 +0300

    представления с помощью классов

--- form_project/feedback/views.py
+++ form_project/feedback/views.py
@@ -2,6 +2,76 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import FeedbackForm
 from .models import Feedback
+from django.views import View
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView
+
+
+class FeedBackView(View):
+    def get(self, request):
+        form = FeedbackForm()
+        return render(request, 'feedback/feedback.html', context={'form': form})
+
+    def post(self, request):
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/done')
+        return render(request, 'feedback/feedback.html', context={'form': form})
+
+
+class UpdateFeedBackView(View):
+    def get(self, request, id_feedback):
+        feed = Feedback.objects.get(id=id_feedback)
+        form = FeedbackForm(instance=feed)
+        return render(request, 'feedback/feedback.html', context={'form': form})
+
+    def post(self, request, id_feedback):
+        feed = Feedback.objects.get(id=id_feedback)
+        form = FeedbackForm(request.POST, instance=feed)
+        if form.is_valid():
+            form.save()
+        return render(request, 'feedback/feedback.html', context={'form': form})
+
+
+class DoneView(TemplateView):
+        template_name = 'feedback/done.html'
+
+        def get_context_data(self, **kwargs):
+            context = super().get_context_data(**kwargs)
+            context['name'] = 'Ivan'
+            context['date'] = '15.06.23'
+            return context
+
+
+# class ListFeedBack(TemplateView):
+#     template_name = 'feedback/list_feedback.html'
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['feedbacks'] = Feedback.objects.all()
+#         return context
+
+
+class ListFeedBack(ListView):
+    template_name = 'feedback/list_feedback.html'
+    model = Feedback
+    context_object_name = 'feedbacks'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        filter_qs = queryset.filter(rating__gt=3)
+        return filter_qs
+
+
+class DetailFeedBack(TemplateView):
+    template_name = 'feedback/detail_feedback.html'
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['feedback'] = Feedback.objects.get(id=kwargs['id_feedback'])
+        return context
 
 
 def index(request):
